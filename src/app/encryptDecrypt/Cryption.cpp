92dbfd1cb14f43fbb5731cfaf40e7e155a41faaf
VANSHAJB10 <vanshajbajaj1002@gmail.com>
2024-07-03 09:07:47 +0530

    added decryption logic

--- src/app/encryptDecrypt/Cryption.cpp
+++ src/app/encryptDecrypt/Cryption.cpp
@@ -23,6 +23,15 @@ int executeCryption(const std::string& taskData) {
         }
         task.f_stream.close();
     }
+    else {
+        char ch;
+        while (task.f_stream.get(ch)) {
+            ch = ch ^ (key + 10);
+            task.f_stream.seekp(-1, std::ios::cur);
+            task.f_stream.put(ch);
+        }
+        task.f_stream.close();
+    }
+    return 0;
 
-    
 }    
